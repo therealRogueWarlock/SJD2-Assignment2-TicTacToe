1cbeacd3117239c6f89ad44d7d4714ac1caa78bf
Nivq <30695715+Nivq@users.noreply.github.com>
2021-03-08 15:30:42 +0100

    Grimme fxml filer

--- src/client/GUI/view/LoginViewController.java
+++ src/client/GUI/view/LoginViewController.java
@@ -1,5 +1,14 @@
 package client.GUI.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginViewController
 {
+    @FXML public TextField gamerTagLabel;
+
+    public void confirmGamerTag(ActionEvent actionEvent)
+    {
+    }
 }

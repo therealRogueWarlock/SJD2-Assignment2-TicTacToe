b2d71d3a18d4477bd584c211754f2be5dcfc1f19
Nivq <30695715+Nivq@users.noreply.github.com>
2021-03-08 16:15:29 +0100

    GameRoom

--- src/client/gui/view/GameRoomViewController.java
+++ src/client/gui/view/GameRoomViewController.java
@@ -1,5 +1,15 @@
 package client.gui.view;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class GameRoomViewController
 {
+    public ListView chatGameRoom;
+    public TextField textToSendGameRoom;
+
+    public void sendTextGameButton(ActionEvent actionEvent)
+    {
+    }
 }

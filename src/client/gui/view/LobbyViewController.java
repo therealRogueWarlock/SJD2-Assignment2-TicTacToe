d34c80424f2c48f8d0fad8aae78e650a346b1039
Nivq <30695715+Nivq@users.noreply.github.com>
2021-03-08 16:08:22 +0100

    LobbyView FXML + Controller setup

--- src/client/gui/view/LobbyViewController.java
+++ src/client/gui/view/LobbyViewController.java
@@ -1,5 +1,33 @@
 package client.gui.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class LobbyViewController
 {
+
+    @FXML public TableColumn listRoomNumber;
+    @FXML public TableColumn listPlayerAmount;
+    @FXML public TableColumn listPlayerNames;
+    @FXML public ListView lobbyChat;
+    @FXML public TextField textToSendLobby;
+
+    public void joinGame(ActionEvent actionEvent)
+    {
+    }
+
+    public void hostGame(ActionEvent actionEvent)
+    {
+    }
+
+    public void quitGame(ActionEvent actionEvent)
+    {
+    }
+
+    public void sendTextLobby(ActionEvent actionEvent)
+    {
+    }
 }

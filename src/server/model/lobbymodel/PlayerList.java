e47424dd6f18a756b19c64ed24b64764d80ec9ba
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-03-11 00:59:59 +0100

    Basic list handling

--- src/server/model/lobbymodel/PlayerList.java
+++ src/server/model/lobbymodel/PlayerList.java
@@ -3,10 +3,17 @@ package server.model.lobbymodel;
 import java.util.HashMap;
 
 public class PlayerList {
+	private HashMap<Integer, String> players;
 
-	private HashMap<String,Integer> players;
+	public void addPlayer(int id, String name) {
+		players.put(id, name);
+	}
 
+	public void removePlayer(int id) {
+		players.remove(id);
+	}
 
-
-
+	public String getName(int id) {
+		return players.get(id);
+	}
 }

f04a188a84cdcbb9c38aab5f38f41eaf42437cce
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-03-11 01:01:25 +0100

    Basic implementation

--- src/server/model/chatmodel/ChatRoom.java
+++ src/server/model/chatmodel/ChatRoom.java
@@ -1,16 +1,18 @@
 package server.model.chatmodel;
 
 import transferobjects.Message;
+
 import java.util.ArrayList;
 
 public class ChatRoom {
+	private ArrayList<Message> allMessages;
 
-	public void addMessage() {
-
+	public void addMessage(Message msg) {
+		allMessages.add(msg);
 	}
 
 	public ArrayList<Message> getAllMessages() {
-		return null;
+		return allMessages;
 	}
 
 }

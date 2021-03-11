526db2cde7c4ccad23b4caef7fd8f4262ff5d4d2
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-03-11 01:07:04 +0100

    Class made for TicTacToe Piece Transfer

---
+++ src/transferobjects/TicTacToePiece.java
@@ -0,0 +1,12 @@
+package transferobjects;
+
+public class TicTacToePiece {
+	int x, y;
+	char piece;
+
+	public TicTacToePiece(int x, int y, char piece) {
+		this.x = x;
+		this.y = y;
+		this.piece = piece;
+	}
+}

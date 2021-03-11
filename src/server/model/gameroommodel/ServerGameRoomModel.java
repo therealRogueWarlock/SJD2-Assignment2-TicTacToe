15795ea9f50bf936dbe2d06b1347e8c3dbda32a3
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-03-11 01:03:50 +0100

    Basic implementation

--- src/server/model/gameroommodel/ServerGameRoomModel.java
+++ src/server/model/gameroommodel/ServerGameRoomModel.java
@@ -1,27 +1,26 @@
 package server.model.gameroommodel;
 
-import util.GameRoomModel;
-import server.model.gamemodel.TicTacToe;
-import client.networking.Client;
 import server.model.chatmodel.ChatRoom;
+import server.model.gamemodel.TicTacToe;
 import transferobjects.Message;
+import util.GameRoomModel;
 
 public class ServerGameRoomModel implements GameRoomModel {
 
 	private TicTacToe ticTacToe;
-
 	private ChatRoom chatRoom;
 
-	public void placePiece() {
-
+	@Override
+	public void placePiece(int x, int y, char piece) {
+		ticTacToe.placePiece(x, y, piece);
 	}
 
-	public void addMessage() {
-
+	public void addMessage(Message msg) { // Er dette ikke bare sendMessage() igen??
+		chatRoom.addMessage(msg);
 	}
 
 	public void sendMessage(Message message) {
-
+		chatRoom.addMessage(message);
 	}
 
 	@Override
